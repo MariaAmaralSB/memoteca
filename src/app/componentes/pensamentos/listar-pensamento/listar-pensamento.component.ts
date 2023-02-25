92c061e82e12961d9dd548b7e7dd38b2b565d2fc
Maria Eduarda Amaral <44277668+MariaAmaralSB@users.noreply.github.com>
2023-02-25 13:44:53 -0300

    comunicação entre componentes

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [];
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedadde é decorada com @Input()',
+      autoria: 'componente filho',
+      modelo: 'modelo2'
+    }
+  ];
 
   constructor() { }
 
